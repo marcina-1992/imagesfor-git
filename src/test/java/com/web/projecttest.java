f8004c33e0287a9b9f92303c08c38574c6f0ba24
marcina doj <marcinadoj@gmail.com>
2025-02-05 10:35:02 +0530

    Messages added to my maven project

--- src/test/java/com/web/projecttest.java
+++ src/test/java/com/web/projecttest.java
@@ -8,13 +8,18 @@ import org.testng.annotations.Test;
 public class projecttest {
 	@Test
 	public void pagetest() throws InterruptedException {
+		
 		System.setProperty("webdriver.driver.chrome","E:\\Seleniumeclipse\\chrome\\chromedriver-win64\\chrome.driver");
 		WebDriver driver=new ChromeDriver();
 		driver.get("file:///E:/new/imagesfor%20git/index.html");
 		Thread.sleep(2000);
 		driver.manage().window().maximize();
 		System.out.println(driver.findElement(By.tagName("a")).getText());
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("")).click();
 		System.out.println("maven process completed successfully");
+		
+		System.out.println("hi welome to my maven project");
 
 }
 }
